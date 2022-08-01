526e87108e57f9140e2359b40578615731c98628
Vancouvertec <vancouvertec@gmail.com>
2022-08-01 00:36:25 -0300

    Colado o projeto no repositorio

---
+++ src/app/Http/Controllers/CursoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\curso;
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\curso  $curso
+     * @return \Illuminate\Http\Response
+     */
+    public function show(curso $curso)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\curso  $curso
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(curso $curso)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\curso  $curso
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, curso $curso)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\curso  $curso
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(curso $curso)
+    {
+        //
+    }
+}
---
+++ src/database/seeders/CursoSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\curso;
+use Illuminate\Database\Seeder;
+
+class CursoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+}
